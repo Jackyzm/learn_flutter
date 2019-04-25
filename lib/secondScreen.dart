af36c51259da975ed677183da730680ac80bd70e
jackyzm <jackyzm@foxmail.com>
2019-04-25 19:14:19 +0800

    feat: add icon && test webview

--- lib/secondScreen.dart
+++ lib/secondScreen.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class SecondScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // appBar: AppBar(
+      //     title: Icon(IconData(0xe607, fontFamily: 'iconfont'),
+      //         color: Colors.green)),
       appBar: AppBar(
-        title: Text("Second Screen"),
-      ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            // Navigate back to the first screen by popping the current route
-            // off the stack
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
-        ),
+          title: Text(
+        "\uE607",
+        style: TextStyle(
+            fontFamily: "iconfont", fontSize: 24.0, color: Colors.green),
+      )),
+      body: WebView(
+        initialUrl: 'https://flutter.dev',
+        javascriptMode: JavascriptMode.unrestricted,
       ),
     );
   }
